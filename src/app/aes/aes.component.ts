226ed6abb6c8002fff703a9c987b4297b48e5e69
Raimund Ege <raimund@ege.com>
2019-12-16 13:06:13 -0800

    updates AES with new dialog style

--- src/app/aes/aes.component.ts
+++ src/app/aes/aes.component.ts
@@ -16,6 +16,8 @@ export class AesComponent implements OnInit {
   request: Request;
   // aesForm: FormGroup;
   spinner = "no";
+  file: any;
+  filemsg: string;
   message: string;
 
   constructor(public http: HttpClient, public authService: AuthService, public dialog: MatDialog) {
@@ -29,11 +31,11 @@ export class AesComponent implements OnInit {
     this.request.keySizeString = "128";
     this.request.ivSize = 128; // DES block size
     this.request.mode = "ECB";
-    this.request.keyStyle = "password"; 
+    this.request.keyStyle = "password";
     // console.log("AesComponent ngOnInit, request: " + JSON.stringify(this.request));
   }
 
-  onSubmit() {    
+  onSubmit() {
     this.request.keySize = +this.request.keySizeString;
     this.request.ivSize = 128; // AES block size
     if (this.gotKey()) {
@@ -56,13 +58,46 @@ export class AesComponent implements OnInit {
     );
   }
 
-  checkFile() {
-   // console.log("checkFile: " + JSON.stringify(this.request))
-   // console.log("file name: " + this.request.fileName + ", uploaded: " + this.request.uploadComplete);
-    if (this.request.uploadComplete == false)
+  check() {
+    this.message = '';
+  //  console.log("check: " , this.request)
+    if (this.request.uploadComplete == false) {     
+      this.message = "file upload is not complete";
       return false;
-    else
-      return this.request.fileName !== undefined;
+    } else {
+      if (this.request.fileName == undefined) {
+        this.message = "file selection is required";
+        return false;
+      }
+    }
+    if (this.request.keyStyle == 'password' && (this.request.password == undefined || this.request.password == '')) {
+      this.message = 'password is required';
+      return false;
+    }
+    if (this.request.keyStyle == 'hex') {
+      if (this.request.keyData == undefined || this.request.keyData == '') {
+        this.message = 'key data in hex format is required';
+        return false;
+      } else {
+        if (this.request.keyData.length !== parseInt(this.request.keySizeString,10)/4) {
+          this.message = 'key data must be specified as ' + parseInt(this.request.keySizeString,10)/8 + ' hex digits';
+          return false;
+        }
+      }
+      if (this.request.mode !== 'ECB') {
+        if (this.request.ivData == undefined || this.request.ivData == '') {
+          this.message = 'iv data in hex format is required';
+          return false;
+        } else {
+          if (this.request.ivData.length !== this.request.ivSize/4) {
+            this.message = 'iv data must be specified as ' + this.request.ivSize/8 + ' hex digits';
+            return false;
+          }
+        }
+      }
+    }
+      
+    return true;
   }
 
   openDialog(map) {
@@ -91,6 +126,36 @@ export class AesComponent implements OnInit {
 
   resetKey() {
     this.authService.selectedKey = null;
+  }
+
+  setFile(event: any) {
+    let fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      this.file = fileList[0];
+      this.request.fileName = this.file.name;
+      this.request.uploadComplete = false;
+      console.log('set upload file: ' + this.file.name + ' - ' + this.file.size + ' bytes');
+
+      let formData: FormData = new FormData();
+      formData.append('userId', '' + this.request.userId);
+      formData.append('file', this.file, this.file.name);
+      const httpOptions = { headers: new HttpHeaders({ 'Accept': 'application/json' }) };
+      // show progress
+      this.filemsg = "uploading ...";
+      this.http.post(SecureURL + '/upload', formData, httpOptions)
+        .subscribe((data: any) => {
+          // turn off progress spinner
+          this.filemsg = data.status;
+          console.log('filemsg: ', this.filemsg)
+          this.request.uploadComplete = true;
+          this.authService.refreshFiles();
+        },
+          error => {
+            this.filemsg = "upload failed: try again later";
+            this.request.uploadComplete = false;
+            console.log(error);
+          })
+    }
   }
 
 }
